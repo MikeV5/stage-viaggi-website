4e909a2ef41cc6c785617a9e34f82e279f56f6fd
miguel <miguel142v@gmail.com>
2024-05-22 13:05:47 +0200

    Aggiunto stato `loading` per tracciare il caricamento dei dati.

--- src/components/AllSchede.js
+++ src/components/AllSchede.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col, Card } from "antd";
+import { Row, Col, Card, Spin } from "antd";
 import { db } from './firebase';
 import { ref, get } from 'firebase/database';
 
 const AllSchede = () => {
     const [schede, setSchede] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const schedeRef = ref(db, 'schede');
@@ -32,23 +33,43 @@ const AllSchede = () => {
             })
             .catch((error) => {
                 console.error("Errore nel recupero delle schede:", error);
+            })
+            .finally(() => {
+                setLoading(false);
             });
     }, []);
 
+    if (loading) {
+        return (
+            <div style={{ textAlign: 'center', marginTop: 50 }}>
+                <Spin size="large" />
+            </div>
+        );
+    }
+
     return (
         <div>
-            <h1>Tutte le schede</h1>
-            <Row gutter={[16, 16]}>
-                {schede.map((scheda) => (
-                    <Col xs={24} sm={12} md={8} lg={6} xl={4} key={scheda.id}>
-                        <Link to={`/edit-scheda?scheda=${scheda.id}`}>
-                            <Card title={scheda.titolo}>
-                                <p>{scheda.autore}</p>
-                            </Card>
-                        </Link>
-                    </Col>
-                ))}
-            </Row>
+            {schede.length === 0 ? (
+                <>
+                    <h1>Non ci sono schede disponibili...</h1>
+                    <p><Link to="/add-scheda">Carica una nuova scheda</Link>.</p>
+                </>
+            ) : (
+                <>
+                    <h1>Tutte le schede</h1>
+                    <Row gutter={[16, 16]}>
+                        {schede.map((scheda) => (
+                            <Col xs={24} sm={12} md={8} lg={6} xl={4} key={scheda.id}>
+                                <Link to={`/edit-scheda?scheda=${scheda.id}`}>
+                                    <Card title={scheda.titolo}>
+                                        <p>{scheda.autore}</p>
+                                    </Card>
+                                </Link>
+                            </Col>
+                        ))}
+                    </Row>
+                </>
+            )}
         </div>
     );
 }
