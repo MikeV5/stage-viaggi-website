016314aee1b27e6a255226e4f74b54848c164d44
miguel <miguel142v@gmail.com>
2024-10-22 23:14:28 +0200

    Test

--- src/components/Content.js
+++ src/components/Content.js
@@ -122,7 +122,7 @@ const Content = () => {
                         </Button>
                     </div>
                     <Button type="primary" block onClick={fetchSchede} style={{ marginLeft: '8px' }}>
-                        Cerca
+                        Cerca2
                     </Button>
                 </Card>
             </Col>
